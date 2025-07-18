92bef71079c239c0e8ea094cf7d181ba88a8acef
Sineka <sinekarajendran03@gmail.com>
2025-07-18 14:17:52 +0530

    local changes update

--- app/Controllers/Adminv4.php
+++ app/Controllers/Adminv4.php
@@ -18,7 +18,7 @@ class Adminv4 extends BaseController
         $userType = $this->session->get('user_type');
 
         if ($userType == 4 || in_array($userType, [3, 5])) {
-            return redirect()->to('/stock');
+            return redirect()->to('/stock/stockdashboard');
         }
 
         if ($userType == 7) {
@@ -72,9 +72,9 @@ class Adminv4 extends BaseController
 
             // Redirect based on user type
             if ($result->user_type == 4) {
-                return redirect()->to('/stock');
+                return redirect()->to('/stock/stockdashboard');
             } elseif (in_array($result->user_type, [3, 5])) {
-                return redirect()->to('/stock');
+                return redirect()->to('/stock/stockdashboard');
             } elseif ($result->user_type == 7) {
                 return redirect()->to('/tppublisher');
             } else {
