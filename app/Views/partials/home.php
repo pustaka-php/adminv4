f1e3350cd18b76d2da91c32eeac48efd4f470d09
Pustaka-Digital-Media <corporate@pustaka.co.in>
2025-11-20 15:51:19 +0530

    ho

--- app/Views/partials/home.php
+++ app/Views/partials/home.php
@@ -297,7 +297,7 @@
         <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 layout-spacing">
             <div class="widget widget-table-two">
                 <div class="widget-heading text-center">
-                    <h5>Pustaka Paperback Orders
+                    <h5>Pustaka Paperback Orders Ship
                         <a href="<?= base_url(); ?>author/author_royalty_list" class="bs-tooltip" title="Author Royalty" target="_blank">
                             <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="blue" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                                 <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2p"></path>
